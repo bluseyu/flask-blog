039cb106b3d46356d8f4dd79ff172a5343c1fbac
bruce <92cg@163.com>
2022-08-31 17:57:03 +0800

    分类管理功能实现

--- client/src/main.js
+++ client/src/main.js
@@ -6,6 +6,7 @@ import { createDiscreteApi } from 'naive-ui'
 import { router } from './common/router'
 import { createPinia } from "pinia";
 import axios from 'axios'
+import { AdminStore } from './stores/AdminStore'
 
 // 访问服务端地址
 axios.defaults.baseURL = "http://localhost:8080"
@@ -23,5 +24,19 @@ app.provide("dialog", dialog)
 app.use(naive)
 app.use(createPinia())
 app.use(router)
+
+/**
+ * 拦截器功能实现
+ * 注入adminStore函数后进行实例化
+ * 每一个i请求都会先执行这段代码，给headers添加一个token，token来自 adminStore
+ * 登录成功后，就有token值传入，每一个请求都携带一个token。
+ * 也可以不用全局，则在每个需要的地方写一次，相对比较麻烦。
+ * 使用token拦截，需要先载入Pinia，故代码必须放在 `app.use(createPinia())`之后
+*/
+const adminStore = AdminStore()
+axios.interceptors.request.use((config) => {
+    config.headers.token = adminStore.token
+    return config
+})
 
 app.mount('#app')
--- client/src/views/dashboard/Category.vue
+++ client/src/views/dashboard/Category.vue
@@ -1,11 +1,155 @@
 <template>
     <div>
-        分类管理
+        <n-button @click="showAddModel = true">添加分类</n-button>
+        <n-table :bordered="false" :single-line="false">
+            <thead>
+                <tr>
+                    <th>编号</th>
+                    <th>名称</th>
+                    <th>操作</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(category, index) in categoryList">
+                    <td>{{  category.id  }}</td>
+                    <td>{{  category.name  }}</td>
+                    <td>
+                        <n-space>
+                            <n-button @click="toUpdate(category)">修改</n-button>
+                            <n-button @click="deleteCategory(category)">删除</n-button>
+                        </n-space>
+                    </td>
+                </tr>
+            </tbody>
+        </n-table>
+
+        <n-modal v-model:show="showAddModel" preset="dialog" title="Dialog">
+            <template #header>
+                <div>添加分类</div>
+            </template>
+            <div>
+                <n-input v-model:value="addCategory.name" type="text" placeholder="请输入名称"></n-input>
+            </div>
+            <template #action>
+                <div>
+                    <n-button @click="add">提交</n-button>
+                </div>
+            </template>
+        </n-modal>
+
+        <n-modal v-model:show="showUpdateModel" preset="dialog" title="Dialog">
+            <template #header>
+                <div>修改分类</div>
+            </template>
+            <div>
+                <n-input v-model:value="updateCategory.name" type="text" placeholder="请输入名称" />
+            </div>
+            <template #action>
+                <div>
+                    <n-button @click="update">提交</n-button>
+                </div>
+            </template>
+        </n-modal>
     </div>
 </template>
 
 <script setup>
 
+import { AdminStore } from '../../stores/AdminStore'
+import { ref, reactive, inject, onMounted } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
+const router = useRouter()
+const route = useRoute()
+
+const message = inject("message")
+const dialog = inject("dialog")
+const axios = inject("axios")
+
+const adminStore = AdminStore()
+
+const showAddModel = ref(false)
+const showUpdateModel = ref(false)
+
+const categoryList = ref([])
+const addCategory = reactive({
+    name: ""
+})
+
+const updateCategory = reactive({
+    id: 0,
+    name: ""
+})
+
+onMounted(() => {
+    loadDatas()
+})
+
+const loadDatas = async () => {
+    let res = await axios.get("/category/list")
+    categoryList.value = res.data.rows
+}
+
+// 添加分类
+const add = async () => {
+    /**
+     * 需要将登录后的Token传给服务端，通过服务端的验证
+     * 问题：每一次发现需要用到Token，都需要加一个头(headers)，最好是封装一个方法，需要的时候自动添加 headers。
+     * 使用拦截器可以满足上述需求，因为需要全局使用，故写在main.js文件中，
+     * 使用拦截器后，就无需添加headers了
+    */
+    //let res = await axios.post("/category/_token/add", { name: addCategory.name }, { headers: { token: adminStore.token } })
+    let res = await axios.post("/category/_token/add", { name: addCategory.name })
+    // 添加成功后列表自动更新，提示相应的消息
+    if (res.data.code == 200) {
+        // 需要重新读一遍列表
+        loadDatas()
+        message.info(res.data.msg)
+    } else {
+        message.error(res.data.msg)
+    }
+    // 不管是否添加成功，隐藏添加弹出框
+    showAddModel.value = false;
+}
+
+// 点击按钮时修改框弹出，弹窗会复制点击的name
+const toUpdate = async (category) => {
+    showUpdateModel.value = true
+    updateCategory.id = category.id
+    updateCategory.name = category.name
+}
+
+// 提交修改数据
+const update = async () => {
+    let res = await axios.put("/category/_token/update", { id: updateCategory.id, name: updateCategory.name })
+    if (res.data.code == 200) {
+        loadDatas()
+        message.info(res.data.msg)
+    } else {
+        message.error(res.data.msg)
+    }
+    showUpdateModel.value = false;
+}
+
+// 删除分类 为防止误删，删除前需要先确认
+const deleteCategory = async (category) => {
+
+    dialog.warning({
+        title: '警告',
+        content: '是否要删除',
+        positiveText: '确定',
+        negativeText: '取消',
+        onPositiveClick: async () => {
+            let res = await axios.delete(`/category/_token/delete?id=${category.id}`)
+            if (res.data.code == 200) {
+                loadDatas()
+                message.info(res.data.msg)
+            } else {
+                message.error(res.data.msg)
+            }
+        }
+    })
+
+}
 </script>
 
 <style lang="scss" scoped>
